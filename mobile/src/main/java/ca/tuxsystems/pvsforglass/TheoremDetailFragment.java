1b8bf637ea02f49d04a994d4fdadf6cbd8a86815
Matthew Dawson <matthew@mjdsystems.ca>
2014-09-22 14:31:44 -0400

    Initial commit of PVSForGlass.

---
+++ mobile/src/main/java/ca/tuxsystems/pvsforglass/TheoremDetailFragment.java
@@ -0,0 +1,62 @@
+package ca.tuxsystems.pvsforglass;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import ca.tuxsystems.pvsforglass.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Theorem detail screen.
+ * This fragment is either contained in a {@link TheoremListActivity}
+ * in two-pane mode (on tablets) or a {@link TheoremDetailActivity}
+ * on handsets.
+ */
+public class TheoremDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public TheoremDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_theorem_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.theorem_detail)).setText(mItem.content);
+        }
+
+        return rootView;
+    }
+}
