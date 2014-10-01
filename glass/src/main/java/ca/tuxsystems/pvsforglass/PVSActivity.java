99af9b72f1a6d11c3b777ee0f18aa159d22057d5
Matthew Dawson <matthew@mjdsystems.ca>
2014-10-01 14:00:35 -0400

    Initial slightly broken zoom.

--- glass/src/main/java/ca/tuxsystems/pvsforglass/PVSActivity.java
+++ glass/src/main/java/ca/tuxsystems/pvsforglass/PVSActivity.java
@@ -1,5 +1,6 @@
 package ca.tuxsystems.pvsforglass;
 
+import com.google.android.glass.touchpad.GestureDetector;
 import com.google.android.glass.widget.CardScrollView;
 
 import android.app.Activity;
@@ -7,7 +8,9 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 
 import min3d.core.Object3dContainer;
 import min3d.core.RendererActivity;
@@ -15,6 +18,7 @@ import min3d.objectPrimitives.Sphere;
 import min3d.vos.RenderType;
 
 import static java.lang.Math.cos;
+import static java.lang.Math.max;
 import static java.lang.Math.sin;
 import static java.lang.Math.toRadians;
 
@@ -36,6 +40,29 @@ public class PVSActivity extends RendererActivity implements SensorEventListener
 
     private SensorManager mSensorManager;
     private Sensor mRotationVector;
+    private GestureDetector detector;
+    private float cameraDistance;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        detector = new GestureDetector(this);
+        detector.setScrollListener(new GestureDetector.ScrollListener() {
+            @Override
+            public boolean onScroll(float displacement, float delta, float velocity) {
+                Log.i("SCROLLER", "D " + displacement + " DE " + delta + " V " + velocity);
+                cameraDistance += delta / 250;
+                cameraDistance = max(0f, cameraDistance);
+
+                scene.camera().position.x = scene.camera().target.x * cameraDistance;
+                scene.camera().position.y = scene.camera().target.y * cameraDistance;
+                scene.camera().position.z = scene.camera().target.z * cameraDistance;
+
+                return true;
+            }
+        });
+    }
 
     @Override
     public void initScene()
@@ -115,11 +142,17 @@ public class PVSActivity extends RendererActivity implements SensorEventListener
         scene.camera().target.x = (float) cos(toRadians(event.values[0]));
         scene.camera().target.z = (float) sin(toRadians(event.values[0]));
         scene.camera().target.y = (float) -cos(toRadians(event.values[1]));
+        scene.camera().target.normalize();
+
+        scene.camera().position.x = scene.camera().target.x * cameraDistance;
+        scene.camera().position.y = scene.camera().target.y * cameraDistance;
+        scene.camera().position.z = scene.camera().target.z * cameraDistance;git 
 
         //Log.i("TYPE_ORIENTATION 0, 1, 2", event.values[0] + " " + event.values[1] + " " + event.values[2]);
 
-        if (System.currentTimeMillis()/100 != m_time) {
-            m_time = System.currentTimeMillis()/100;
+        int d = 10000;
+        if (System.currentTimeMillis()/d != m_time) {
+            m_time = System.currentTimeMillis()/d;
         Log.i("CAM TARGET", scene.camera().target.toString());
         }
     }
@@ -127,6 +160,11 @@ public class PVSActivity extends RendererActivity implements SensorEventListener
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        return detector.onMotionEvent(event) && super.onGenericMotionEvent(event);
     }
 
     long m_time = 0;
